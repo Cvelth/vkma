39f81e97bd5dfe9e604024c3842cd47640ad25cc
Cvelth <cvelth.mail@gmail.com>
2021-01-19 11:29:25 +0200

    Update the bindings include strategy

--- vkma.hpp
+++ vkma.hpp
@@ -37,7 +37,6 @@
 #include <system_error>
 #include <tuple>
 #include <type_traits>
-#include <vk_mem_alloc.h>
 
 #if 17 <= VKMA_CPP_VERSION
 #  include <string_view>
@@ -100,25 +99,60 @@ extern "C" __declspec( dllimport ) FARPROC __stdcall GetProcAddress( HINSTANCE h
 #    define VMA_IMPLEMENTATION
 #  endif
 
-#  include <vk_mem_alloc.h>
+/*
+In every place where you want to use Vulkan Memory Allocator, define appropriate
+macros if you want to configure the library and then include its header to
+include all public interface declarations. Example:
+*/
 
-#  ifdef VMA_RECORDING_ENABLED
-#    define VKMA_RECORDING_ENABLED VMA_RECORDING_ENABLED
+//#define VMA_HEAVY_ASSERT(expr) assert(expr)
+//#define VMA_USE_STL_CONTAINERS 1
+//#define VMA_DEDICATED_ALLOCATION 0
+//#define VMA_DEBUG_MARGIN 16
+//#define VMA_DEBUG_DETECT_CORRUPTION 1
+//#define VMA_DEBUG_INITIALIZE_ALLOCATIONS 1
+//#define VMA_RECORDING_ENABLED 1
+//#define VMA_DEBUG_MIN_BUFFER_IMAGE_GRANULARITY 256
+//#define VMA_USE_STL_SHARED_MUTEX 0
+//#define VMA_DEBUG_GLOBAL_MUTEX 1
+//#define VMA_MEMORY_BUDGET 0
+//#define VMA_STATIC_VULKAN_FUNCTIONS 0
+//#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
+
+//#define VMA_VULKAN_VERSION 1002000 // Vulkan 1.2
+//#define VMA_VULKAN_VERSION 1001000 // Vulkan 1.1
+//#define VMA_VULKAN_VERSION 1000000 // Vulkan 1.0
+
+/*
+#define VMA_DEBUG_LOG(format, ...) do { \
+        printf(format, __VA_ARGS__); \
+        printf("\n"); \
+    } while(false)
+*/
+
+#  ifdef _MSVC_LANG
+#    pragma warning( push, 4 )
+#    pragma warning( disable : 4127 )  // conditional expression is constant
+#    pragma warning( disable : 4100 )  // unreferenced formal parameter
+#    pragma warning( disable : 4189 )  // local variable is initialized but not referenced
+#    pragma warning( disable : 4324 )  // structure was padded due to alignment specifier
 #  endif
-#  ifdef VMA_VULKAN_VERSION
-#    define VKMA_VULKAN_VERSION VMA_VULKAN_VERSION
+#  ifdef __clang__
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wtautological-compare"  // comparison of unsigned expression < 0 is always false
+#    pragma clang diagnostic ignored "-Wunused-private-field"
+#    pragma clang diagnostic ignored "-Wunused-parameter"
+#    pragma clang diagnostic ignored "-Wmissing-field-initializers"
+#    pragma clang diagnostic ignored "-Wnullability-completeness"
 #  endif
-#  ifdef VMA_DEDICATED_ALLOCATION
-#    define VKMA_DEDICATED_ALLOCATION VMA_DEDICATED_ALLOCATION
+
+#  include "vk_mem_alloc.h"
+
+#  ifdef __clang__
+#    pragma clang diagnostic pop
 #  endif
-#  ifdef VMA_BIND_MEMORY2
-#    define VKMA_BIND_MEMORY2 VMA_BIND_MEMORY2
-#  endif
-#  ifdef VMA_MEMORY_BUDGET
-#    define VKMA_MEMORY_BUDGET VMA_MEMORY_BUDGET
-#  endif
-#  ifdef VMA_STATS_STRING_ENABLED
-#    define VKMA_STATS_STRING_ENABLED VMA_STATS_STRING_ENABLED
+#  ifdef _MSVC_LANG
+#    pragma warning( pop )
 #  endif
 
 #  define VKMA_NULL_HANDLE VK_NULL_HANDLE
@@ -657,6 +691,7 @@ inline void vkmaGetImageAllocation( VkmaImage image, VkmaAllocation * pAllocatio
 // *pDefragmentationStats)
 
 #endif  // VKMA_BINDINGS_HPP
+
 // The end of an external bindings file.
 
 // 32-bit vulkan is not typesafe for handles, so don't allow copy constructors on this platform by default.
